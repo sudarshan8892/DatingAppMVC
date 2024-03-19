8f7cf70af6883ff8b80cbddc8636fa6d8aca00d0
mforazure9@gmail.com <mforazure9@gmail.com>
2024-03-19 21:23:38 +0530

    UI

---
+++ HS.Web.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HS.Web.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
