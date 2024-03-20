1ae134521b6e55fe0e98756df627bc210d9d2f40
mforazure9@gmail.com <mforazure9@gmail.com>
2024-03-20 15:13:55 +0530

    new controller

---
+++ HS.Web.UI/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebAPIDatingAPP.Entities;
+
+namespace HS.Web.UI.Controllers
+{
+    public class UsersController : Controller
+    {
+        Uri baseUrl = new Uri("https://localhost:9999/api");
+        private readonly HttpClient _Client;
+
+        public UsersController()
+        {
+            _Client = new HttpClient();    
+            _Client.BaseAddress = baseUrl;  
+        }
+
+        public IActionResult Index()
+        {
+            string data = null;
+            HttpResponseMessage response = _Client.GetAsync(_Client.BaseAddress + "/AppUsers").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                 data = response.Content.ReadAsStringAsync().Result;
+
+            }
+            var appUsers = JsonConvert.DeserializeObject<IEnumerable<AppUsers>>(data);
+            return View("Index", appUsers);
+        }
+    }
+}
